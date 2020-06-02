05fd7576e43795e15ba4c98ac905e33ccd409be6
Curtis Chavez <cchavez539@cnm.edu>
2020-06-02 10:17:05 -0600

    Version one completed. (sort of)

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.animals.controller;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -12,7 +13,13 @@ import android.view.ViewGroup;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.R;
+import edu.cnm.deepdive.animals.model.Animal;
+import edu.cnm.deepdive.animals.service.AnimalService;
+import java.io.IOException;
+import java.util.List;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -44,13 +51,14 @@ public class ImageFragment extends Fragment {
     settings.setDisplayZoomControls(false);
     settings.setUseWideViewPort(true);
     settings.setLoadWithOverviewMode(true);
-
+    new Retriever().start();
   }
 
   private class Retriever extends Thread {
 
     @Override
     public void run() {
+
       Gson gson = new GsonBuilder()
           .excludeFieldsWithoutExposeAnnotation()
           .create();
@@ -58,7 +66,35 @@ public class ImageFragment extends Fragment {
           .baseUrl("https://us-central1-apis-4674e.cloudfunctions.net/")
           .addConverterFactory(GsonConverterFactory.create(gson))
           .build();
+
+      AnimalService animalService = retrofit.create(AnimalService.class);
+
+      try {
+        Response<List<Animal>> response = animalService.getAnimals(BuildConfig.CLIENT_KEY)
+            .execute();
+
+        if (response.isSuccessful()) {
+          List<Animal> animals = response.body();
+
+          assert animals != null;
+          final String url = animals.get(0).getUrl();
+          getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+              Log.d("AnimalService", url);
+              contentView.loadUrl(url);
+            }
+          });
+        } else {
+          Log.e("AnimalService", response.message());
+        }
+
+      } catch (IOException e) {
+        Log.e("AnimalService", e.getMessage(), e);
+      }
+
     }
+
   }
 
 }

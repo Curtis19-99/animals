4b1f4a5b1eddb31f038bebd82368efea96c5034a
Curtis Chavez <cchavez539@cnm.edu>
2020-06-08 16:19:42 -0600

    Added Spinner.

--- app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/ImageFragment.java
@@ -7,6 +7,10 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -18,7 +22,6 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import edu.cnm.deepdive.animals.BuildConfig;
 import edu.cnm.deepdive.animals.MainViewModel;
 import edu.cnm.deepdive.animals.R;
 import edu.cnm.deepdive.animals.model.Animal;
@@ -29,16 +32,21 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ImageFragment extends Fragment {
+public class ImageFragment extends Fragment implements OnItemSelectedListener {
 
   private WebView contentView;
   private MainViewModel viewModel;
+
+  private Spinner spinner;
+  private List <Animal> animals;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_image, container, false);
     setupWebView(root);
+    spinner = root.findViewById(R.id.animals_spinner);
+    spinner.setOnItemSelectedListener(this);
     return root;
   }
 
@@ -51,7 +59,12 @@ public class ImageFragment extends Fragment {
     viewModel.getAnimals().observe(getViewLifecycleOwner(), new Observer<List<Animal>>() {
       @Override
       public void onChanged(List<Animal> animals) {
-        contentView.loadUrl(animals.get(49).getUrl());
+        ImageFragment.this.animals = animals;
+        ArrayAdapter<Animal> adapter= new ArrayAdapter<>(
+            ImageFragment.this.getContext(), R.layout.custom_spinner_item, animals
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
       }
     });
   }
@@ -74,4 +87,13 @@ public class ImageFragment extends Fragment {
   }
 
 
+  @Override
+  public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+    contentView.loadUrl(animals.get(pos).getUrl());
+  }
+
+  @Override
+  public void onNothingSelected(AdapterView<?> adapterView) {
+
+  }
 }
